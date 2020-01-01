48e6320ded5f5f69a565d7d10692a02558c251b7
zhanghz1120 <uL2YgYwhTUF7ldwh>
2020-01-01 19:01:31 +0800

    基本操作

---
+++ src/main/java/tree/_2_binary_tree_postorder_traversal.java
@@ -0,0 +1,39 @@
+package tree;
+
+import java.util.*;
+
+/**
+ * 求给定的二叉树的后序遍历。迭代解法
+ *
+ *
+ *
+ *
+ *
+ */
+public class _2_binary_tree_postorder_traversal {
+    public ArrayList<Integer> postorderTraversal(TreeNode root) {
+        ArrayList<Integer> ret = new ArrayList<Integer>();
+        if(root == null){
+            return ret;
+        }
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        stack.push(root);
+        while(!stack.empty()){
+            TreeNode node = stack.pop();
+            ret.add(node.val);
+
+            if(node.left != null){
+                stack.push(node.left);
+            }
+            if(node.right != null){
+                stack.push(node.right);
+            }
+        }
+
+        Collections.reverse(ret);
+        return ret;
+    }
+
+
+
+}
