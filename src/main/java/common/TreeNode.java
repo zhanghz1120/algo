02fdcfa919f362c7f3a70a2633a8c02c69ada991
zhanghanzhi_cd <zhanghz1120@foxmail.com>
2020-01-22 15:19:53 +0800

    基本操作

--- src/main/java/common/TreeNode.java
+++ src/main/java/common/TreeNode.java
@@ -1,5 +1,7 @@
 package common;
 
+import java.util.*;
+
 public class TreeNode {
     public int val;
 
@@ -8,6 +10,47 @@ public class TreeNode {
 
     public TreeNode(int val) {
         this.val = val;
+    }
+
+    public static ArrayList<ArrayList<Integer>> printTree(TreeNode root){
+        ArrayList<ArrayList<Integer>> ret = new ArrayList<ArrayList<Integer>>();
+        if(root == null){
+            return ret;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root);
+        // 记录当前行有多少还在队列中
+        int x = 1;
+
+        // 记录下一行有多少在队列中
+        int y = 0;
+
+        ArrayList<Integer> buffer = new ArrayList<Integer>();
+        while(queue.size() > 0){
+            TreeNode node = queue.poll();
+            x--;
+            buffer.add(node.val);
+
+            if(node.left != null){
+                queue.offer(node.left);
+                y++;
+            }
+            if(node.right != null){
+                queue.offer(node.right);
+                y++;
+            }
+            if(x == 0){
+                x = y;
+                y = 0;
+                ret.add(buffer);
+                buffer = new ArrayList<Integer>();
+            }
+        }
+
+        //Collections.reverse(ret);
+        return ret;
+
     }
 
 }
--- src/main/java/tree/_13_construct_binary_tree_from_inorder_and_postorder_traversal.java
+++ src/main/java/tree/_13_construct_binary_tree_from_inorder_and_postorder_traversal.java
@@ -1,6 +1,7 @@
 package tree;
 
 import common.TreeNode;
+import org.junit.Test;
 
 import java.util.*;
 
@@ -12,10 +13,39 @@ import java.util.*;
  */
 public class _13_construct_binary_tree_from_inorder_and_postorder_traversal {
     public TreeNode buildTree(int[] inorder, int[] postorder) {
-        
+        return build(inorder, postorder, 0, inorder.length-1, 0, postorder.length-1);
+    }
 
+    public TreeNode build(int[] inorder, int[] post_order, int i1, int j1, int i2, int j2){
+        if(i1 == j1 || i2 == j2){
+            return new TreeNode(inorder[i1]);
+        }
 
-        return null;
+        TreeNode root = new TreeNode(post_order[j2]);
+        int mid = i1;
+        for(int i = i1; i<=j1; i++){
+            if(inorder[i] == root.val){
+                mid = i;
+                break;
+            }
+        }
+
+        TreeNode left = build(inorder, post_order, i1, mid-1, i2, mid-1);
+        TreeNode right = build(inorder, post_order, mid+1, j1, mid, j2-1);
+        root.left = left;
+        root.right = right;
+        return root;
+    }
+
+    @Test
+    public void testBuildTree(){
+        _13_construct_binary_tree_from_inorder_and_postorder_traversal obj = new _13_construct_binary_tree_from_inorder_and_postorder_traversal();
+
+        int[] inorder = {5, 0, 6, 1, 2, 8, 3};
+        int[] post_order = {5, 6, 0, 1, 8, 3, 2};
+        TreeNode root = obj.buildTree(inorder, post_order);
+
+        //System.out.println(Arrays.toString(TreeNode.printTree(root).toArray()));
     }
 
 }
