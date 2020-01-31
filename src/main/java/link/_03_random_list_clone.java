8e2e6c7295d83d726b05b1b6b65df95c0db13667
zhanghz1120 <uL2YgYwhTUF7ldwh>
2020-01-31 22:18:51 +0800

    基本操作

--- src/main/java/link/_03_random_list_clone.java
+++ src/main/java/link/_03_random_list_clone.java
@@ -13,9 +13,35 @@ import common.RandomListNode;
 
 public class _03_random_list_clone {
     public RandomListNode Clone(RandomListNode pHead) {
+        if(pHead == null){
+            return null;
+        }
 
+        RandomListNode current = pHead;
+        while(current != null){
+            RandomListNode bro = new RandomListNode(current.label);
+            RandomListNode next = current.next;
+            current.next = bro;
+            bro.next = next;
+            current = next;
+        }
 
-        return null;
+        current = pHead;
+        while(current != null){
+            RandomListNode bro = current.next;
+            bro.random = current.random == null ? null : current.random.next;
+            current = bro.next;
+        }
+
+        current = pHead;
+        RandomListNode head = current.next;
+        while(current != null){
+            RandomListNode bro = current.next;
+            current.next = bro.next;
+            bro.next = bro.next == null ? null : bro.next.next;
+            current = current.next;
+        }
+        return head;
     }
 
 }
---
+++ src/main/java/other/_03_convert_bst_to_sorted_linked_list.java
@@ -0,0 +1,16 @@
+package other;
+
+import common.TreeNode;
+
+/**
+ * 输入一棵二叉搜索树，将该二叉搜索树转换成一个排序的双向链表。
+ * 要求不能创建任何新的结点，只能调整树中结点指针的指向。
+ *
+ *
+ */
+public class _03_convert_bst_to_sorted_linked_list {
+    public TreeNode Convert(TreeNode root) {
+        return null;
+    }
+
+}
